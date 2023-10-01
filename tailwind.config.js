50f2f00fabb8a99e322d3c155b0251e1e9ea5e03
Krushil <krushilnaik96@gmail.com>
2023-10-01 17:51:51 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "media",
+  content: ["./index.html", "./src/**/*.vue"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
